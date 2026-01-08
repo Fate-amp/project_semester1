6adfb83f5913bbe0b6e86a5ec984cbb0b6b27f93
Artyom-Y <artem.iavchunovskii@epita.fr>
2026-01-08 14:43:07 +0100

    query.py for exporting data from mysql server
    i made a simple setup, where you can use functions from query.py to get data from mysql server in form of a pandas dataframe

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import query
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("base.html")
+
+
+# example on how to easily get data from the database
+connection = query.get_connection()
+get_query = query.get_query(connection, "select product_name from dataset limit 5")
+print(get_query)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ query.py
@@ -0,0 +1,35 @@
+from sqlalchemy import URL, create_engine, text
+import pandas as pd
+
+# enter your localhost information to connect to the database
+username = "root"
+password = "admin"
+host = "127.0.0.1"
+port = 3306
+database_name = "cosmetics_shop"
+
+
+url_object = URL.create(
+    drivername="mysql+pymysql",
+    username=username,
+    password=password,
+    host=host,
+    port=port,
+    database=database_name,
+)
+
+
+# return connection object
+def get_connection(url_object=url_object):
+    engine = create_engine(url_object)
+    connection = engine.connect()
+    return connection
+
+
+# return DataFrame object of the query
+def get_query(connection, query):
+    result = connection.execute(text(f"{query}"))
+    return pd.DataFrame(result)
+
+
+# TODO function to return pandas datadrame based on CursorResult object
